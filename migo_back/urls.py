50376fe6c6bc888bd45e68ba659f6f2b5508fb45
Adrian Miranda <adrian.miranda.r@gmail.com>
2025-11-21 23:08:36 -0300

    se cambia nombre de adminstracion de django a administracion de migo

--- migo_back/urls.py
+++ migo_back/urls.py
@@ -4,6 +4,12 @@ URL configuration for migo_back project.
 from django.contrib import admin
 from django.urls import path, include
 
+# Personalizar títulos del admin
+admin.site.site_header = "Administración de MIGO"
+admin.site.site_title = "MIGO Admin"
+admin.site.index_title = "Panel de Administración"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
